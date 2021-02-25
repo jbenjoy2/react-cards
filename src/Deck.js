6a336c0ee97e935f28bf70e4b5331be6db6175a5
Jason Benjoya <jasonbenjoya@gmail.com>
2021-02-24 19:44:54 -0500

    removed duplicate code

--- src/Deck.js
+++ src/Deck.js
@@ -9,58 +9,8 @@ const Deck = () => {
 	const [ deck, setDeck ] = useState(null);
 	const [ drawn, setDrawn ] = useState([]);
 	const [ autoDraw, setAutoDraw ] = useState(false);
-	const timerRef = useRef();
-	// on page load, go get new deck and save it to state! only do once!
-	useEffect(
-		() => {
-			async function getDeck() {
-				let deck = await axios.get(`${API}/new/shuffle/`);
-				setDeck(deck.data);
-			}
-			getDeck();
-		},
-		[ setDeck ]
-	);
+	const timerRef = useRef(null);
 
-	useEffect(
-		() => {
-			async function drawCard() {
-				const { deck_id } = deck;
-
-				try {
-					const draw = await axios.get(`${API}/${deck_id}/draw/`);
-					if (draw.data.remaining === 0 && !draw.data.success) {
-						setAutoDraw(false);
-						throw new Error('No more cards!');
-					}
-					const card = draw.data.cards[0];
-					setDrawn((d) => [
-						...d,
-						{
-							id    : card.code,
-							value : card.value,
-							suit  : card.suit,
-							image : card.image
-						}
-					]);
-				} catch (error) {
-					alert(error);
-				}
-			}
-			if (autoDraw && !timerRef.current) {
-				timerRef.current = setInterval(async () => {
-					await drawCard();
-				}, 1000);
-			}
-
-			return () => {
-				clearInterval(timerRef.current);
-				timerRef.current = null;
-			};
-		},
-		[ autoDraw, setAutoDraw, deck ]
-	);
-	// grab a new card each time the button is clicked!
 	const drawCard = async () => {
 		try {
 			const { deck_id } = deck;
@@ -82,6 +32,34 @@ const Deck = () => {
 			alert(error);
 		}
 	};
+	// on page load, go get new deck and save it to state! only do once!
+	useEffect(
+		() => {
+			async function getDeck() {
+				let deck = await axios.get(`${API}/new/shuffle/`);
+				setDeck(deck.data);
+			}
+			getDeck();
+		},
+		[ setDeck ]
+	);
+
+	useEffect(
+		() => {
+			if (autoDraw && !timerRef.current) {
+				timerRef.current = setInterval(async () => {
+					await drawCard();
+				}, 1000);
+			}
+
+			return () => {
+				clearInterval(timerRef.current);
+				timerRef.current = null;
+			};
+		},
+		[ autoDraw, setAutoDraw, deck ]
+	);
+	// grab a new card each time the button is clicked!
 
 	const toggleAutoDraw = () => {
 		setAutoDraw((a) => !a);
